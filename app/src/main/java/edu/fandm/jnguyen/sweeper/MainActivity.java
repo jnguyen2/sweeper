2530f3b539e4eb8ceb80749d8a0697a6386c3425
Joseph Nguyen <jnguyen2@fandm.edu>
2019-03-20 17:44:01 -0400

    Implement basic ticking

--- app/src/main/java/edu/fandm/jnguyen/sweeper/MainActivity.java
+++ app/src/main/java/edu/fandm/jnguyen/sweeper/MainActivity.java
@@ -1,13 +1,57 @@
 package edu.fandm.jnguyen.sweeper;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String TAG = "sweeper.MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void bombClick(View v) {
+        activateBomb((Button) v);
+    }
+
+    public void activateBomb(Button bomb) {
+        BombTimer bombTimer = new BombTimer(bomb);
+        bombTimer.execute();
+    }
+
+    class BombTimer extends AsyncTask<Void, Void, Void> {
+        private Button bomb;
+
+        BombTimer(Button bomb) {
+            super();
+            this.bomb = bomb;
+        }
+
+        @Override
+        protected void onPreExecute() {
+            bomb.setBackgroundColor(getResources().getColor(R.color.miscRed));
+        }
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+            try {
+                Thread.sleep(500);
+            } catch (Exception e) {
+                Log.d(TAG, "I guess the bomb ticks forever....");
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void result) {
+            bomb.setBackgroundColor(getResources().getColor(R.color.miscBlack));
+        }
     }
 }
