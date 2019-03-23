1ca46108e46caf9c05cbabdf641fba0faafc68c4
Joseph Nguyen <jnguyen2@fandm.edu>
2019-03-23 18:20:54 -0400

    Extend game functionality

--- app/src/main/java/edu/fandm/jnguyen/sweeper/MainActivity.java
+++ app/src/main/java/edu/fandm/jnguyen/sweeper/MainActivity.java
@@ -1,13 +1,12 @@
 package edu.fandm.jnguyen.sweeper;
 
-import android.os.AsyncTask;
 import android.os.Handler;
 import android.os.SystemClock;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
@@ -17,22 +16,28 @@ public class MainActivity extends AppCompatActivity {
     public static final String TAG = "sweeper.MainActivity";
 
     private ArrayList<Button> bombs = new ArrayList<>();
+    private SweeperGame sweeperGame;
 
     // Game ticks.
-    private int count = 0;
+    private int clock = 0;
     private boolean isTicking = false;
     private Handler handler = new Handler();
     private TickTask tickTask = new TickTask();
 
     public void startGame(View v) {
-        if (isTicking) {
-            isTicking = false;
-            handler.removeCallbacks(tickTask);
-            count = 0;
-        } else {
+        ticking(!isTicking);
+    }
+
+    public void ticking(boolean on) {
+        if (on) {
             handler.removeCallbacks(tickTask);
             handler.postDelayed(tickTask, 1000);
             isTicking = true;
+            sweeperGame.newGame();
+        } else {
+            isTicking = false;
+            handler.removeCallbacks(tickTask);
+            clock = 0;
         }
     }
 
@@ -40,16 +45,41 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         public void run() {
-            updateUI();
+            tick();
             long start = SystemClock.uptimeMillis();
             handler.postAtTime(this, start + 1000);
         }
     }
 
+    private void tick() {
+        updateUI();
+        sweeperGame.tick();
+        clock++;
+    }
+
     private void updateUI() {
-        Button bomb = bombs.get(0);
-        bomb.setText(Integer.toString(count));
-        count++;
+        if (sweeperGame.isGameOver()) {
+            ticking(false);
+            indicateLoss();
+        }
+
+        TextView clockTV = findViewById(R.id.textView_clock);
+        clockTV.setText(Integer.toString(clock));
+
+        for (int i = 0; i < bombs.size(); i++) {
+            Button bomb = bombs.get(i);
+            if (sweeperGame.bombIsActive(i)) {
+                bomb.setBackgroundColor(getResources().getColor(R.color.miscRed));
+            } else if (sweeperGame.bombExploded(i)) {
+                bomb.setBackgroundColor(getResources().getColor(R.color.miscBlack));
+            } else {
+                bomb.setBackgroundColor(getResources().getColor(R.color.miscWhite));
+            }
+        }
+    }
+
+    private void indicateLoss() {
+        Toast.makeText(this, "You lost!", Toast.LENGTH_SHORT).show();
     }
 
     @Override
@@ -57,18 +87,20 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ButtonGrid buttonGrid = findViewById(R.id.buttonGrid);
+        sweeperGame = SweeperGame.getInstance(getApplicationContext());
 
-        Integer bombCount = getResources().getInteger(R.integer.bomb_count);
-        for (int i = 0; i < bombCount; i++) {
+        ButtonGrid buttonGrid = findViewById(R.id.buttonGrid);
+        for (int i = 0; i < sweeperGame.getBombCount(); i++) {
             final int idx = i;
             Button button = buttonGrid.getButton(i);
             button.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                    Toast.makeText(getApplicationContext(), String.format("Button %d pressed.", idx), Toast.LENGTH_SHORT).show();
+                    sweeperGame.defuseBomb(idx);
+                    updateUI();
                 }
             });
+            bombs.add(button);
         }
     }
 }
--- app/src/main/java/edu/fandm/jnguyen/sweeper/SweeperGame.java
+++ app/src/main/java/edu/fandm/jnguyen/sweeper/SweeperGame.java
@@ -1,33 +1,33 @@
 package edu.fandm.jnguyen.sweeper;
 
 import android.content.Context;
+import android.util.Log;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.Map;
 import java.util.Random;
 
 public class SweeperGame {
 
+    public static final String TAG = "sweeper.sweepergame";
+
     private static SweeperGame sweeperGame;
     private Random random = new Random();
-    private int clock;
     private int timeUntilNextBomb;
     private int bombCount;
+    private int bombsRemaining;
+    private HashSet<Integer> bombsExploded;
     private ArrayList<Integer> bombTimers = new ArrayList<>();
     private int mainFuseTime = 10;
-    private int currentLevel = 0;
+    private int currentLevel = 1;
 
     private SweeperGame(Context context) {
         bombCount = context.getResources().getInteger(R.integer.bomb_count);
-
-        // -1 means not exploded or ticking, 0 is exploded.
-        for (int i = 0; i < bombCount; i++) {
-            bombTimers.add(-1);
-        }
     }
 
     // Return the one and only instance of SweeperGame.
-    public SweeperGame getInstance(Context context) {
+    public static SweeperGame getInstance(Context context) {
         if (sweeperGame == null) {
             sweeperGame = new SweeperGame(context);
         }
@@ -35,28 +35,78 @@ public class SweeperGame {
         return sweeperGame;
     }
 
-    public int getBombTimer(int index) {
-        return bombTimers.get(index);
+    public boolean isGameOver() {
+        return bombsRemaining <= 0;
+    }
+
+    public int getBombCount() {
+        return bombCount;
+    }
+
+    public boolean bombIsActive(int index) {
+        return bombTimers.get(index) > 0;
+    }
+
+    public boolean bombExploded(int index) {
+        return bombTimers.get(index) == 0;
+    }
+
+    public void defuseBomb(int index) {
+        if (bombTimers.get(index) == 0) {
+            return;
+        }
+
+        if (bombIsActive(index)) {
+            bombTimers.set(index, -1);
+        } else if (!bombExploded(index)){
+            bombTimers.set(index, 0);
+            bombsRemaining--;
+        }
     }
 
     public void newGame() {
-        clock = 0;
+        bombsRemaining = bombCount;
         timeUntilNextBomb = 0;
+
+        // -1 means not exploded or ticking, 0 is exploded.
+        bombTimers = new ArrayList<>();
+        for (int i = 0; i < bombCount; i++) {
+            bombTimers.add(-1);
+        }
     }
 
-    public void tickOnce() {
-        clock++;
+    public void tick() {
         timeUntilNextBomb--;
 
-        if (timeUntilNextBomb == 0) {
+        if (timeUntilNextBomb <= 0) {
             // Find an inactive bomb.
-            int index = random.nextInt(bombCount);
-            while (bombTimers.get(index) != -1) {
+            Integer index = 0;
+            if (bombsRemaining > 1) {
                 index = random.nextInt(bombCount);
+                while (bombsRemaining > 0 && (bombIsActive(index) || bombExploded(index))) {
+                    index = random.nextInt(bombCount);
+                }
+            } else {
+                for (int i = 0; i < bombCount; i++) {
+                    if (!bombExploded(i)) {
+                        index = i;
+                        break;
+                    }
+                }
             }
 
             // Activate!
             bombTimers.set(index, mainFuseTime - currentLevel);
+            timeUntilNextBomb = random.nextInt(mainFuseTime / 2);
+        }
+
+        for (int i = 0; i < bombCount; i++) {
+            if (bombIsActive(i)) {
+                bombTimers.set(i, bombTimers.get(i) - 1);
+                if (bombExploded(i)) {
+                    bombsRemaining--;
+                }
+            }
         }
     }
 }
