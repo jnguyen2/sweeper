9254131108cbeb58c2adafdf7194774f4b5c9960
Joseph Nguyen <jnguyen2@fandm.edu>
2019-03-23 22:24:13 -0400

    Improve handling of non-exploded bombs

--- app/src/main/java/edu/fandm/jnguyen/sweeper/SweeperGame.java
+++ app/src/main/java/edu/fandm/jnguyen/sweeper/SweeperGame.java
@@ -1,11 +1,8 @@
 package edu.fandm.jnguyen.sweeper;
 
 import android.content.Context;
-import android.util.Log;
 
 import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.Map;
 import java.util.Random;
 
 public class SweeperGame {
@@ -14,20 +11,18 @@ public class SweeperGame {
 
     private static SweeperGame sweeperGame;
     private Random random = new Random();
-    private int timeUntilNextBomb;
+    private int lives;
+    private int bombsDefused;
     private int bombCount;
-    private int bombsRemaining;
-    private HashSet<Integer> bombsExploded;
     private ArrayList<Integer> bombTimers = new ArrayList<>();
-    private int mainFuseTime = 10;
-    private int currentLevel = 1;
+    private ArrayList<Integer> notExplodedIndices = new ArrayList<>();
 
     private SweeperGame(Context context) {
         bombCount = context.getResources().getInteger(R.integer.bomb_count);
     }
 
     // Return the one and only instance of SweeperGame.
-    public static SweeperGame getInstance(Context context) {
+    static SweeperGame getInstance(Context context) {
         if (sweeperGame == null) {
             sweeperGame = new SweeperGame(context);
         }
@@ -35,77 +30,78 @@ public class SweeperGame {
         return sweeperGame;
     }
 
-    public boolean isGameOver() {
-        return bombsRemaining <= 0;
+    boolean isGameOver() {
+        return notExplodedIndices.isEmpty() || lives <= 0;
     }
 
-    public int getBombCount() {
+    int getBombCount() {
         return bombCount;
     }
 
-    public boolean bombIsActive(int index) {
+    int getScore() {
+        return bombsDefused;
+    }
+
+    boolean bombIsActive(int index) {
         return bombTimers.get(index) > 0;
     }
 
-    public boolean bombExploded(int index) {
-        return bombTimers.get(index) == 0;
+    boolean bombExploded(int index) {
+        return !notExplodedIndices.contains(index);
     }
 
-    public void defuseBomb(int index) {
-        if (bombTimers.get(index) == 0) {
+    void defuseBomb(int index) {
+        if (bombExploded(index)) {
             return;
         }
 
         if (bombIsActive(index)) {
             bombTimers.set(index, -1);
-        } else if (!bombExploded(index)){
+            bombsDefused++;
+        } else if (!bombExploded(index)) {
             bombTimers.set(index, 0);
-            bombsRemaining--;
+            lives--;
+            notExplodedIndices.remove(Integer.valueOf(index));
         }
     }
 
-    public void newGame() {
-        bombsRemaining = bombCount;
-        timeUntilNextBomb = 0;
+    void newGame() {
+        // Reset lives and score.
+        lives = 3;
+        bombsDefused = 0;
 
-        // -1 means not exploded or ticking, 0 is exploded.
-        bombTimers = new ArrayList<>();
+        // Clear the lists.
+        bombTimers.clear();
+        notExplodedIndices.clear();
+
         for (int i = 0; i < bombCount; i++) {
+            // -1 means not exploded or ticking, 0 is exploded.
             bombTimers.add(-1);
+
+            // Also, keep track of indices still functional.
+            notExplodedIndices.add(i);
         }
     }
 
-    public void tick() {
-        timeUntilNextBomb--;
-
-        if (timeUntilNextBomb <= 0) {
-            // Find an inactive bomb.
-            Integer index = 0;
-            if (bombsRemaining > 1) {
-                index = random.nextInt(bombCount);
-                while (bombsRemaining > 0 && (bombIsActive(index) || bombExploded(index))) {
-                    index = random.nextInt(bombCount);
-                }
-            } else {
-                for (int i = 0; i < bombCount; i++) {
-                    if (!bombExploded(i)) {
-                        index = i;
-                        break;
-                    }
-                }
+    void tick() {
+        int desired = random.nextInt(notExplodedIndices.size() / 2);
+        // Activate an inactive bomb.
+        while (desired > 0) {
+            Integer index = notExplodedIndices.get(random.nextInt(notExplodedIndices.size()));
+            if (!bombIsActive(index)) {
+                bombTimers.set(index, 5);
             }
-
-            // Activate!
-            bombTimers.set(index, mainFuseTime - currentLevel);
-            timeUntilNextBomb = random.nextInt(mainFuseTime / 2);
+            desired--;
         }
 
         for (int i = 0; i < bombCount; i++) {
-            if (bombIsActive(i)) {
-                bombTimers.set(i, bombTimers.get(i) - 1);
-                if (bombExploded(i)) {
-                    bombsRemaining--;
-                }
+            int timeRemaining = bombTimers.get(i);
+            bombTimers.set(i, timeRemaining - 1);
+
+            // The bomb just exploded.
+            if (timeRemaining - 1 == 0) {
+                lives--;
+                notExplodedIndices.remove(Integer.valueOf(i));
             }
         }
     }
